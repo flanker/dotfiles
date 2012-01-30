f74fc28083ea277fbe32abd9fa0bb1e70b450269
FENG Zhichao <flankerfc@gmail.com>
2012-01-30 14:11:24 +1100

    update bash shell

--- bash/proxy.bash
+++ bash/proxy.bash
@@ -5,8 +5,8 @@ function proxy () {
   if [ -n "$1" ]; then
     state=$1
     echo "set web & secure web proxy $state"
-    networksetup -setwebproxystate AirPort $state
-    networksetup -setsecurewebproxystate AirPort $state
+    networksetup -setwebproxystate Wi-Fi $state
+    networksetup -setsecurewebproxystate Wi-Fi $state
     networksetup -setwebproxystate Ethernet $state
     networksetup -setsecurewebproxystate Ethernet $state
     dscacheutil -flushcache
--- bash_profile
+++ bash_profile
@@ -19,7 +19,7 @@ set -o vi
 
 PS1="\[\033[36m\]\u\[\033[33m\]@\[\033[35m\]\W\[\033[m\]\$ "
 
-PATH=/usr/local/bin:$PATH
+PATH=/usr/local/bin:/usr/local/mongodb/bin:$PATH
 
 # load all bash files
 for f in ~/.dotfiles/bash/*.bash
