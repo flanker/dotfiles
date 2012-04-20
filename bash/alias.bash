d4e400afa1c4c613606a613bc05c64f72c2cd48a
FENG Zhichao <flankerfc@gmail.com>
2012-04-20 13:45:28 +0800

    do not need password to stop mysql in alias

--- bash/alias.bash
+++ bash/alias.bash
@@ -10,7 +10,7 @@ alias dev='cd ~/Dev'
 
 # alias for application
 alias mysqlstart='sudo /usr/local/mysql/bin/mysqld_safe --user=mysql'
-alias mysqlstop='/usr/local/mysql/bin/mysqladmin -u root -p shutdown'
+alias mysqlstop='/usr/local/mysql/bin/mysqladmin -u root shutdown'
 alias tomcatstart='/usr/local/tomcat/bin/startup.sh'
 alias tomcatstop='/usr/local/tomcat/bin/shutdown.sh'
 alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
