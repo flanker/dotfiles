2168c9db03de93d3a74dd36a3cd231a796b33237
FENG Zhichao <flankerfc@gmail.com>
2014-04-25 12:00:52 +0800

    Settings for Sublime Text

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,11 @@ timestamp=`date +"%m%d%H%M%S"`
 bak_dir=.dotfiles_bak/$timestamp/
 mkdir -p $bak_dir
 
-mv .bash_profile $bak_dir
-mv .gitconfig $bak_dir
+mv ~/.bash_profile $bak_dir
+mv ~/.gitconfig $bak_dir
+mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User $bak_dir
 
 ln -s ~/Dev/mygithub/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Dev/mygithub/dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/Dev/mygithub/dotfiles/sublime/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+ln -s ~/Dev/mygithub/dotfiles/sublime/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
